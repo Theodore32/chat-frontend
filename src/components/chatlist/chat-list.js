be848e13bd8efd2209e8742efbc625a5a54100ce
theoo <theodorechristopher32@gmail.com>
2018-10-19 11:44:44 +0700

    udah bisa chat

--- src/components/chatlist/chat-list.js
+++ src/components/chatlist/chat-list.js
@@ -3,7 +3,8 @@ import './chatlist.css';
 
 import gambar from '../../picture/boy.png';
 import {
-  recieveChat
+  recieveChat,
+  sendChat
 }from "../../socket/socketconnect";
 
 export default class FriendList extends React.Component{
@@ -12,27 +13,51 @@ export default class FriendList extends React.Component{
 
     this.state = {
       chatlog:[],
+      openchat:false,
       lastMessage : '',
       notif : 0
     }
   }
 
   componentDidMount(){
-    this.activeSocket(this.props.chat.name)
+    document.addEventListener("keydown", this.escOnClick, false);
+    this.activeSocket(this.props.chat.chatId)
+    this.socketcloseChatroom()
+    this.getChatData(this.props.chat)
   }
   componentWillUnmount(){
-    this.activeSocket(this.props.chat.name)
+    document.removeEventListener("keydown", this.escOnClick, false);
+    this.activeSocket(this.props.chat.chatId)
+    this.socketcloseChatroom()
+  }
+  socketcloseChatroom=()=>{
+    recieveChat('chatroom'+this.props.chat.username,(err,recieve)=>{
+      console.log('from socket',recieve);
+        this.setState({
+          openchat:false
+        })
+    })
   }
   activeSocket(port){
     recieveChat(port,(err,recieve)=>{
       this.setState({
-        chatlog:this.state.chatlog.concat({send:recieve.send,message:recieve.message.message,sender:recieve.message.reciever,reciever:recieve.message.sender,image:recieve.message.image}),
-        lastMessage : recieve.message.message
+        chatlog:this.state.chatlog.concat({message:recieve.message.message,sender:recieve.message.sender,reciever:recieve.message.sender,image:recieve.message.image,time: recieve.message.time}),
+        lastMessage:recieve.message.message,
+        timeStamp :recieve.message.time
       })
-      this.props.changeName(null,null,this.state.chatlog)
+      if(this.state.openchat){
+        this.props.changeName(null,this.props.chatId,this.state.chatlog)
+      }
     })
   }
-
+  escOnClick = (e) =>{
+    if(e.keyCode === 27) {
+      //Do whatever when esc is pressed
+      this.setState({
+        openchat:false
+      })
+    }
+  }
   getChatData = (chat) => {
 
     fetch('/getchat',{
@@ -47,14 +72,18 @@ export default class FriendList extends React.Component{
       .then(json =>{
         if(json.success){
           this.setState({
-            chatlog : json.message
+            chatlog : json.message,
+            lastMessage:json.message.slice(-1).pop().message,
+            timeStamp :json.message.slice(-1).pop().time,
           })
-          this.openChatRoom(chat,this.state.chatlog)
         }
     })
   }
 
   openChatRoom = (item,log) => {
+    this.setState({
+      openchat:true
+    })
     this.props.changeName(item,item.chatId,log);
   }
 
@@ -64,7 +93,7 @@ export default class FriendList extends React.Component{
         <div>
           <li className = "chat-list-text"
             onClick={() =>
-              this.getChatData(chat)
+              this.openChatRoom(chat,this.state.chatlog)
             }
           >
           <div className = "chat-list-picture">
@@ -75,7 +104,7 @@ export default class FriendList extends React.Component{
               {chat.name}
             </div>
             <div className = "friend-time">
-              asd
+              {this.state.timeStamp}
             </div>
           </div>
           <div>
--- src/components/text-message/text-message.js
+++ src/components/text-message/text-message.js
@@ -27,21 +27,29 @@ export default class inputMessage extends React.Component{
 
   onSend(e){
     e.preventDefault();
-
-    const time = this.state.time;
+    const today = new Date();
+    const getHours = (today.getHours() < 10 ? '0' : '') + today.getHours();
+    const getMinute = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
+    const time = getHours+":"+getMinute;
+    const date = today.getDate();
     const message = this.state.message;
     const attachment = this.state.file;
-    console.log("ATTACH: ",attachment);
-    console.log("msg: ",message);
+    // console.log("ATTACH: ",attachment);
+    // console.log("msg: ",message);
     if(this.state.message){
       let send = {
         reciever:this.props.sender,
-        sender:this.props.recieve,
+        sender:{
+          username: this.props.senderUsername,
+          name:this.props.sender
+        },
+        chatId:this.props.chatId,
         message:this.state.message,
-        image:this.state.imagePreviewUrl
+        image:this.state.imagePreviewUrl,
+        time : time
       }
-      sendChat(send)
-      this.handleUserInput(attachment,message,time)
+      sendChat('sendChat',send)
+      this.handleUserInput(attachment,message,time,date)
       this.setState({
         message:'',
         imagePreviewUrl :'',
@@ -58,20 +66,17 @@ export default class inputMessage extends React.Component{
     })
   }
 
-  handleUserInput = (file,message) =>{
-    this.attachPhoto(file,message);
+  handleUserInput = (file,message,time,date) =>{
+    this.attachPhoto(file,message,time,date);
   }
 
-  attachPhoto = (attachment,message) =>{
+  attachPhoto = (attachment,message,time,date) =>{
+    const senderUsername = this.props.senderUsername;
     const sender = this.props.sender;
-    const today = new Date();
-    const getHours = (today.getHours() < 10 ? '0' : '') + today.getHours();
-    const getMinute = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
-    const time = getHours+":"+getMinute;
-    const date = today.getDate();
     const chatId = this.props.chatId
     var formData = new FormData();
-    formData.append ('chatId', chatId)
+    formData.append ('chatId', chatId);
+    formData.append ('senderUsername', senderUsername);
     formData.append ('sender',sender);
     formData.append ('attachment', attachment);
     formData.append ('message', message);
@@ -88,7 +93,8 @@ export default class inputMessage extends React.Component{
         // window.location.reload()
         // this.props.history.push('/ChatRoom')
         this.setState({
-          error : response.message
+          error : response.message,
+          time : response.time
         })
       }
       else{
@@ -97,7 +103,6 @@ export default class inputMessage extends React.Component{
           error: response.message
         })
       }
-      console.log(this.state.error);
     })
   }
 
